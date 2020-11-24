9e7cb95d80e968e884d2535ceb45f0246abc1c53
Marla Glasser <mbglasser@gmail.com>
2020-11-24 16:13:00 -0500

    Populate submitted by as current user full name

--- src/components/AddRecipe/AddRecipe.tsx
+++ src/components/AddRecipe/AddRecipe.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUserFullName } from "../../reducers/currentUser";
 
 import RecipeForm from "../RecipeForm/RecipeForm";
 
-const AddRecipe: React.FC = () => (
-  <RecipeForm savedValues={{ submitted_by: "Lynne Glasser" }} type="add" />
-);
+const AddRecipe: React.FC = () => {
+  const currentUserFullName = useSelector(selectCurrentUserFullName);
+
+  return <RecipeForm savedValues={{ submitted_by: currentUserFullName }} type="add" />;
+};
 
 export default AddRecipe;
