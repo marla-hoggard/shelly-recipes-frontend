22682d17522d9d602c1dae373d8b4b52c4c28410
Marla Glasser <mbglasser@gmail.com>
2021-02-04 01:20:30 -0500

    Hit the backend on homepage load to wake up dynos, edit box shadows.

--- src/components/HomePage/Homepage.tsx
+++ src/components/HomePage/Homepage.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BACKEND_BASE_URL } from '../../constants';
 import TitleSearch from './TitleSearch';
 import CategoryList from './CategoryList';
 import classes from '../../global-styles/base.module.scss';
 
 const Homepage: React.FC = () => {
+  useEffect(() => {
+    // hit the backend on load to wake up dynos
+    fetch(`${BACKEND_BASE_URL}`);
+  }, []);
+
   return (
     <>
       <h1 className={classes.pageTitle}>Glasser Family Recipe Collection</h1>
